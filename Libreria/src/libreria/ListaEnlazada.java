df98201c5b10f0fd5a83f1b9ee15a8d4b3ef2b15
Juan-Felix11 <juanfelixcastillo06@gmail.com>
2025-06-22 14:22:23 -0600

    Se creó la clase ListaEnlazada y sus metodos

---
+++ Libreria/src/libreria/ListaEnlazada.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libreria;
+
+/**
+ *
+ * @author Juan Félix
+ */
+public class ListaEnlazada<T> {
+    //Atributo del primer objeto 
+    private NodoLista<T> cabeza;
+    
+    //Constructor vacío 
+    public ListaEnlazada(){
+        this.cabeza = null;
+    }
+    
+    public void agregar(T dato){
+        NodoLista<T> nuevo = new NodoLista<>(dato);
+        if (cabeza == null){
+            cabeza = nuevo;
+        } else{
+            NodoLista<T> actual = cabeza;
+            while (actual.getSiguiente() != null){
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(nuevo);
+        }
+        
+    }
+    
+    //Método para ver si la lista esta vacia
+    public boolean estavacia(){
+        return cabeza == null;
+    }
+    
+    //Método para eliminar un nodo de la lista 
+    public void eliminar(T dato){
+        if (cabeza == null) return;
+        
+        if (cabeza.getDato().equals(dato)){
+            cabeza = cabeza.getSiguiente();
+            return;
+        }
+        NodoLista<T> actual = cabeza;
+        while (actual.getSiguiente() != null && !actual.getSiguiente().getDato().equals(dato)){
+            actual = actual.getSiguiente();   
+        }
+        if (actual.getSiguiente() != null){
+            actual.setSiguiente(actual.getSiguiente().getSiguiente());
+        }
+    }
+    
+    
+    
+    
+}
