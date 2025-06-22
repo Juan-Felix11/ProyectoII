69fca2c1a7d11a15c63c2cb154ecaadf5d4d7f6c
Juan-Felix11 <juanfelixcastillo06@gmail.com>
2025-06-22 14:24:51 -0600

    Se modifico la clase usuario

--- Libreria/src/libreria/Usuario.java
+++ Libreria/src/libreria/Usuario.java
@@ -9,5 +9,43 @@ package libreria;
  * @author Juan Félix
  */
 public class Usuario {
+    private String nombre; 
+    private ListaEnlazada<String> historial; //Almacena el historial de préstamos (titulos de libros)
+    
+    public Usuario(String nombre){
+        this.nombre = nombre;
+        this.historial = new ListaEnlazada<>();
+    }
+    
+    //Getters
+    public String getNombre(){
+        return this.nombre;
+    }
+    public ListaEnlazada<String> getHistorial(){
+        return this.historial; 
+    }
+    
+    //Setters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    //Agregar un libro al historial 
+    public void agregarPrestamo(String tituloLibro){
+        historial.agregar(tituloLibro); 
+    }
+    
+    //Mostrar el historial como String
+    public String mostrarHistorial(){
+        if (historial.estaVacia()){
+            return "No hay prestamos registrados: ";
+        }
+        return historial.toString();  
+    }
+    
+    @Override
+    public String toString(){
+        return "Nombre: " + nombre;
+    }
     
 }
